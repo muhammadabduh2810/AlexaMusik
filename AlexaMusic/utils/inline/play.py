e1c87a481688c22fc78e52360ef6243b5b377875
muhammadabduh2810 <140913562+muhammadabduh2810@users.noreply.github.com>
2025-04-08 06:22:28 +0700

    Add files via upload

--- AlexaMusic/utils/inline/play.py
+++ AlexaMusic/utils/inline/play.py
@@ -51,7 +51,7 @@ def stream_markup_timer(_, videoid, chat_id, played, dur):
                 text=_["PL_B_2"],
                 callback_data=f"add_playlist {videoid}",
             ),
-            InlineKeyboardButton(text="𝖮𝗐𝗇𝖾𝗋", url=f"https://t.me/Spyzieeskieee"),
+            InlineKeyboardButton(text="𝖮𝗐𝗇𝖾𝗋", url=f"https://t.me/ritonibos"),
         ],
         [
             InlineKeyboardButton(
@@ -84,7 +84,7 @@ def telegram_markup_timer(_, videoid, chat_id, played, dur):
                 text=_["PL_B_2"],
                 callback_data=f"add_playlist {videoid}",
             ),
-            InlineKeyboardButton(text="𝖮𝗐𝗇𝖾𝗋", url=f"https://t.me/Spyzieeskieee"),
+            InlineKeyboardButton(text="𝖮𝗐𝗇𝖾𝗋", url=f"https://t.me/ritonibos"),
         ],
         [
             InlineKeyboardButton(
@@ -107,7 +107,7 @@ def stream_markup(_, videoid, chat_id):
                 text=_["PL_B_2"],
                 callback_data=f"add_playlist {videoid}",
             ),
-            InlineKeyboardButton(text="𝖮𝗐𝗇𝖾𝗋", url=f"https://t.me/Spyzieeskieee")
+            InlineKeyboardButton(text="𝖮𝗐𝗇𝖾𝗋", url=f"https://t.me/ritonibos")
         ],
         [
             InlineKeyboardButton(
